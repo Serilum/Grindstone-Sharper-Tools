207bce63970a0286cfa347f23adee49f32aeb157
Rick South <rick@natamus.com>
2023-12-12 10:03:40 +0100

    Grindstone Sharper Tools source code for 1.19.3.

---
+++ Common/src/main/java/com/natamus/grindstonesharpertools/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.grindstonesharpertools;
+
+import com.natamus.grindstonesharpertools.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
