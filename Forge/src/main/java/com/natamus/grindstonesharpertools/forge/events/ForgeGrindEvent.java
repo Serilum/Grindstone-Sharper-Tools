01d9c4bade592aa5f722ed869a1b0d2d3d1537b5
Rick South <rick@natamus.com>
2023-12-12 10:03:46 +0100

    Grindstone Sharper Tools source code for 1.19.4.

---
+++ Forge/src/main/java/com/natamus/grindstonesharpertools/forge/events/ForgeGrindEvent.java
@@ -0,0 +1,28 @@
+package com.natamus.grindstonesharpertools.forge.events;
+
+import com.natamus.grindstonesharpertools.events.GrindEvent;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeGrindEvent {
+	@SubscribeEvent
+	public void onDamage(LivingHurtEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+
+		float originalDamage = e.getAmount();
+		float newDamage = GrindEvent.onDamage(livingEntity.level, livingEntity, e.getSource(), originalDamage);
+
+		if (originalDamage != newDamage) {
+			e.setAmount(newDamage);
+		}
+	}
+
+	@SubscribeEvent
+	public void onClick(PlayerInteractEvent.RightClickBlock e) {
+		GrindEvent.onClick(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec());
+	}
+}
