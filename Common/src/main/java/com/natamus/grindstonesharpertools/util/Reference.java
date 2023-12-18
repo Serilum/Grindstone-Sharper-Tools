fb48e5fc8284e2118d07dca8c8804c5f5ebe37b1
Rick South <rick@natamus.com>
2023-12-18 10:25:35 +0100

    (v3.4) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/grindstonesharpertools/util/Reference.java
+++ Common/src/main/java/com/natamus/grindstonesharpertools/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.grindstonesharpertools.util;
 public class Reference {
 	public static final String MOD_ID = "grindstonesharpertools";
 	public static final String NAME = "Grindstone Sharper Tools";
-	public static final String VERSION = "3.3";
+	public static final String VERSION = "3.4";
 	public static final String ACCEPTED_VERSIONS = "[1.20.2]";
 }
