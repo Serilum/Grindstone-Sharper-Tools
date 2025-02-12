a7d7535269f1e7207925477431bb2b27a784dc53
Rick South <rick@natamus.com>
2025-02-12 23:30:44 +0100

    (v3.8) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/grindstonesharpertools/forge/events/ForgeGrindEvent.java
+++ Forge/src/main/java/com/natamus/grindstonesharpertools/forge/events/ForgeGrindEvent.java
@@ -5,12 +5,10 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgeGrindEvent {
 	@SubscribeEvent
-	public void onDamage(LivingHurtEvent e) {
+	public static void onDamage(LivingHurtEvent e) {
 		LivingEntity livingEntity = e.getEntity();
 
 		float originalDamage = e.getAmount();
@@ -22,7 +20,7 @@ public class ForgeGrindEvent {
 	}
 
 	@SubscribeEvent
-	public void onClick(PlayerInteractEvent.RightClickBlock e) {
+	public static void onClick(PlayerInteractEvent.RightClickBlock e) {
 		GrindEvent.onClick(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec());
 	}
 }
